9d75b710bf865309e8e258f25b942ff85b356515
SophanonChhoun <sophanon.chhoun@student.niptict.edu.kh>
2021-03-30 22:43:28 +0700

    [IMP]screen_reponsive

--- lib/screens/coursedetail.dart
+++ lib/screens/coursedetail.dart
@@ -189,6 +189,7 @@ class _CourseDetailScreenState extends State<CourseDetailScreen> {
   }
 
   _buildListView() {
+    var size = MediaQuery.of(context).size;
     return Column(
       children: [
         SizedBox(
@@ -254,7 +255,7 @@ class _CourseDetailScreenState extends State<CourseDetailScreen> {
                               mainAxisAlignment: MainAxisAlignment.start,
                               children: [
                                 Container(
-                                  width: 300,
+                                  width: size.width - 150,
                                   child: Align(
                                     alignment: Alignment.centerLeft,
                                     child: Text(
